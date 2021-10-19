92d21668696375d6b733792eb8ae4afdafdfbdc3
Nyah Smith <sminyah@amazon.com>
2021-10-19 16:04:24 -0400

    Changed heading from h2 to h3

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>Hello Nyah!</h1>
-  <h2> Im in NY </h2>
+  <h3> Im in NY </h3>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
